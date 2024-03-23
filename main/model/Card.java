ecdc6b0ade6034024e6adb27d91742e6bbe40800
ryan-davis0 <ryanjd216@gmail.com>
2024-03-23 16:59:21 -0400

    added health stat
    :100:

--- main/model/Card.java
+++ main/model/Card.java
@@ -2,10 +2,12 @@ package main.model;
 public class Card {
     private String name;
     private int attackPoints;
+    private int health;
 
-    public Card(String name, int attackPoints) {
+    public Card(String name, int attackPoints,int health) {
         this.name = name;
         this.attackPoints = attackPoints;
+        this.health = health;
     }
 
     public String getName() {
@@ -15,7 +17,17 @@ public class Card {
     public int getAttackPoints() {
         return attackPoints;
     }
+    public int getHealthPoints(){
+return health;
 
+
+    }
+    public void changeHealth(int change){
+health-=change;
+    }
+public void changeAttackPoints(int change){
+    attackPoints+=change;
+}
     @Override
     public String toString() {
         return name + " (Attack: " + attackPoints + ")";
