2e7c7ee94c38641c7d02814d834c6e382275bf26
Ethan Morton <ethieq909@gmail.com>
2024-03-23 17:04:33 -0400

    merged card

--- main/model/Card.java
+++ main/model/Card.java
@@ -1,7 +1,22 @@
 package main.model;
 
-public record Card(String name, int attackPoints) {
+public class Card {
+    private final String name;
+    private int attackPoints;
+    private int health;
 
+    public Card(String name, int attackPoints,int health) {
+        this.name = name;
+        this.attackPoints = attackPoints;
+        this.health = health;
+    }
+
+    public String getName() {return name;}
+    public int getAttackPoints() {return attackPoints;}
+    public int getHealthPoints(){return health;}
+
+    public void changeHealth(int change){health-=change;}
+    public void changeAttackPoints(int change){attackPoints+=change;}
     @Override
     public String toString() {
         return name + " (Attack: " + attackPoints + ")";
